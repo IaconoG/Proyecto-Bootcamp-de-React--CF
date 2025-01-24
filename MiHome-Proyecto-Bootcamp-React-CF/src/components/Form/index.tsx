55d5df146517d436979022976e9156e420f2035b
Gianfranco Iacono <iacono.gian13@gmail.com>
2025-01-23 23:57:11 -0300

    form not working

--- MiHome-Proyecto-Bootcamp-React-CF/src/components/Form/index.tsx
+++ MiHome-Proyecto-Bootcamp-React-CF/src/components/Form/index.tsx
@@ -1,55 +1,49 @@
 import React, { useState } from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
 import FormMessage from '../../components/FormMessage';
 
 import { FormDataType, FormProps } from './utils/types';
 
 import styles from './Form.module.css';
 
-const Form: React.FC<FormProps> = ({
-  defaultValues,
-  children,
-  onSubmit,
-  formLayout,
-  validationRules = {},
-  resetForm = true,
-}) => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm<FormDataType>({
-    defaultValues,
-  });
+const Form: React.FC<FormProps> = (
+  {
+    // defaultValues,
+    // children,
+    // onSubmit,
+    // formLayout,
+    // validationRules = {},
+    // resetForm = true,
+  }
+) => {
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   formState: { errors },
+  //   reset,
+  // } = useForm<FormDataType>({
+  //   defaultValues,
+  // });
 
   const [isFormUpdated, setIsFormUpdated] = useState<boolean>(false);
 
-  const handleFormSubmit: SubmitHandler<FormDataType> = (data) => {
-    try {
-      onSubmit(data);
-      setIsFormUpdated(true);
-      setTimeout(() => {
-        setIsFormUpdated(false);
-      }, 1500);
-    } catch (error) {
-      console.error('Error al enviar el formulario:', error);
-    }
-    if (resetForm) reset();
-  };
+  // const handleFormSubmit: SubmitHandler<FormDataType> = (data) => {
+  //   try {
+  //     onSubmit(data);
+  //     setIsFormUpdated(true);
+  //     setTimeout(() => {
+  //       setIsFormUpdated(false);
+  //     }, 1500);
+  //   } catch (error) {
+  //     console.error('Error al enviar el formulario:', error);
+  //   }
+  //   if (resetForm) reset();
+  // };
 
   return (
     <>
-      <form
-        onSubmit={handleSubmit(handleFormSubmit)}
-        className={formLayout + ' ' + styles.form}
-      >
+      {/* <form onSubmit={handleSubmit(handleFormSubmit)} className={formLayout + ' ' + styles.form}>
         {React.Children.map(children, (child) => {
-          if (
-            React.isValidElement(child) &&
-            child.props.name &&
-            child.props.type !== 'button'
-          ) {
+          if (React.isValidElement(child) && child.props.name && child.props.type !== 'button') {
             return React.createElement(child.type, {
               ...{
                 ...child.props,
@@ -62,22 +56,16 @@ const Form: React.FC<FormProps> = ({
           }
           return child;
         })}
-        {isFormUpdated && (
-          <FormMessage message="Formulario Actualizado" submit={true} />
-        )}
+        {isFormUpdated && <FormMessage message="Formulario Actualizado" submit={true} />}
 
         {errors && Object.keys(errors).length !== 0 && (
           <>
             {Object.keys(errors).map((key) => (
-              <FormMessage
-                key={key}
-                message={errors[key as keyof FormDataType]['message']}
-                error={true}
-              />
+              <FormMessage key={key} message={errors[key as keyof FormDataType]['message']} error={true} />
             ))}
           </>
         )}
-      </form>
+      </form> */}
     </>
   );
 };

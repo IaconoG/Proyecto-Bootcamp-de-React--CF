0d46179e0556ab62973ba991219bc406ec6ab7c4
Gianfranco Iacono <iacono.gian13@gmail.com>
2025-02-01 17:27:37 -0300

    feat: mejorar el componente TimeSection con estilos responsivos

--- src/components/Sidebar/Header/time-section/index.tsx
+++ src/components/Sidebar/Header/time-section/index.tsx
@@ -4,30 +4,17 @@ import styles from "./TimeSection.module.css";
 
 type TimeSectionProps = {
   className?: string;
+  isCollapsed: boolean;
 };
 
-const TimeSection: React.FC<TimeSectionProps> = ({ className }) => {
+const TimeSection: React.FC<TimeSectionProps> = ({ className, isCollapsed }) => {
   const { currentTime } = useCurrentTime();
 
   return (
-    <div className={`${className || ""} ${styles.timeContainer}`}>
-      <div className={styles.date}>
-        <span>28 Diciembre 2025</span>
-      </div>
-      <div className={styles.hour}>
-        <span>20</span>
-        {/* <span>{currentTime.hours}</span> */}
-      </div>
-      <div className={styles.timeGroup}>
-        <div className={styles.minutes}>
-          <span>45</span>
-          {/* <span>{currentTime.minutes}</span> */}
-        </div>
-        <div className={styles.seconds}>
-          <span>00</span>
-          {/* <span>{currentTime.seconds}</span> */}
-        </div>
-      </div>
+    <div className={`${className || ""} ${styles.timeContainer} ${isCollapsed ? styles.collapsed : ""}`}>
+      <p className={styles.hours}>{currentTime.hours}</p>
+      <p className={styles.minutes}>{currentTime.minutes}</p>
+      <p className={styles.seconds}>{currentTime.seconds}</p>
     </div>
   );
 };

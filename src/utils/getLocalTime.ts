cb7d587a766894e9f9ad9a9e3fd6c7877b6daf5a
Gianfranco Iacono <iacono.gian13@gmail.com>
2025-02-01 21:49:44 -0300

    feat: ajustar formato de hora en getLocalTime para usar el formato de 24 horas

--- src/utils/getLocalTime.ts
+++ src/utils/getLocalTime.ts
@@ -1,5 +1,5 @@
 export const getLocalTime = (timezone: string) => {
-  const now = new Date().toLocaleTimeString("es-AR", { timeZone: timezone });
+  const now = new Date().toLocaleTimeString("es-AR", { timeZone: timezone, hour12: false });
   const [hours, minutes, seconds] = now.split(":");
 
   return { hours, minutes, seconds };

de58ba5678412529d67bcf93ffbf0c5497da39c9
Gianfranco Iacono <iacono.gian13@gmail.com>
2025-02-15 06:07:36 -0300

    feat: agregar componente Toast con estilos y animaciones para notificaciones

---
+++ src/components/Toast/index.tsx
@@ -0,0 +1,35 @@
+import { ToastProps, ToastVariant } from "../../hooks/useToast";
+import styles from "./Toast.module.css";
+
+enum ToastIcons {
+  success = "😊",
+  error = "😞",
+  warning = "💀",
+  info = "🤔",
+}
+
+const Toast = ({ message, variant = ToastVariant.INFO, isFadingOut }: ToastProps) => {
+  return (
+    <div className={`${styles.toast} ${styles[variant]} ${isFadingOut ? styles.fadeOut : ""}`}>
+      {message} {ToastIcons[variant as keyof typeof ToastIcons]}
+    </div>
+  );
+};
+
+const ToastContainer = ({ toasts }: { toasts: ToastProps[] }) => {
+  return (
+    <div className={styles.container}>
+      {toasts.map((toast) => (
+        <Toast
+          key={toast.id}
+          id={toast.id}
+          message={toast.message}
+          variant={toast.variant}
+          isFadingOut={toast.isFadingOut}
+        />
+      ))}
+    </div>
+  );
+};
+
+export { Toast, ToastContainer };

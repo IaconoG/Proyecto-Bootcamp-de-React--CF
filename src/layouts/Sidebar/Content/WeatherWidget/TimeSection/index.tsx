2f8232425dfcfa204da0789d43108a878e6cf3ae
Gianfranco Iacono <iacono.gian13@gmail.com>
2026-02-11 20:50:00 -0300

    style(sidebar): update TimeSection animations and transitions

--- src/layouts/Sidebar/Content/WeatherWidget/TimeSection/index.tsx
+++ src/layouts/Sidebar/Content/WeatherWidget/TimeSection/index.tsx
@@ -13,14 +13,30 @@ const TimeSection: React.FC<TimeSectionProps> = ({
 }) => {
   const { currentTime } = useCurrentTime();
 
+  const hours = parseInt(currentTime.hours);
+  const minutes = parseInt(currentTime.minutes);
+
+  const tenHours = Math.floor(hours / 10);
+  const unitHours = hours % 10;
+
+  const tenMinutes = Math.floor(minutes / 10);
+  const unitMinutes = minutes % 10;
+
   return (
     <div
       className={`${className || ""} ${styles.timeContainer} ${
         isCollapsed ? styles.collapsed : ""
       }`}
     >
-      <p className={styles.hours}>{currentTime.hours}</p>
-      <p className={styles.minutes}>{currentTime.minutes}</p>
+      <div className={styles.hoursContainer}>
+        <p className={styles.hours}>{tenHours}</p>
+        <p className={styles.hours}>{unitHours}</p>
+      </div>
+      {!isCollapsed && <p className={styles.separator}>:</p>}
+      <div className={styles.minutesContainer}>
+        <p className={styles.minutes}>{tenMinutes}</p>
+        <p className={styles.minutes}>{unitMinutes}</p>
+      </div>
     </div>
   );
 };
